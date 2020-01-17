9de89730df82a65e3081df1ce09d4cd6094b1e89
pascal2310 <pascal_96@live.nl>
2020-01-17 20:51:27 +0100

    test

--- Javascript Section/StalkerExercise.js
+++ Javascript Section/StalkerExercise.js
@@ -3,5 +3,5 @@ var lastName = prompt("What is your last name?");
 var age = prompt("what is your age?");
 
 alert("Welcome " + firstName + " " + lastName);
-alert("You look pretty young for " + age + " years")
+alert("You look pretty young for the age of " + age + " years")
 
